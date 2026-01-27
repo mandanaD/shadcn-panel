1bb1a90751861d2e6e128e0bc8d0df5bb08c48cb
Mandana Dashti <dmandana59@gmail.com>
2026-01-27 23:06:53 +0330

    feat: create sidebarData the links and data of sidebar

---
+++ src/components/layout/sidebar/sidebarData.ts
@@ -0,0 +1,187 @@
+import {
+    Construction,
+    LayoutDashboard,
+    Monitor,
+    Bug,
+    ListTodo,
+    FileX,
+    HelpCircle,
+    Lock,
+    Bell,
+    Package,
+    Palette,
+    ServerOff,
+    Settings,
+    Wrench,
+    UserCog,
+    UserX,
+    Users,
+    MessagesSquare,
+    ShieldCheck,
+    Command,
+    LockKeyhole
+} from 'lucide-react'
+import type {SidebarData} from "@/components/layout/sidebar/sidebar.types.ts";
+
+export const sidebarData: SidebarData = {
+    user: {
+        name: 'Mandana',
+        email: 'dmandana59@gmail.com',
+        avatar: '/avatars/shadcn.jpg',
+    },
+    title: {
+        name: 'Shadcn Panel',
+        logo: Command,
+        plan: 'Vite + ShadcnUI',
+    },
+    navGroups: [
+        {
+            title: 'General',
+            items: [
+                {
+                    title: 'Dashboard',
+                    url: '/',
+                    icon: LayoutDashboard,
+                },
+                {
+                    title: 'Tasks',
+                    url: '/tasks',
+                    icon: ListTodo,
+                },
+                {
+                    title: 'Apps',
+                    url: '/apps',
+                    icon: Package,
+                },
+                {
+                    title: 'Chats',
+                    url: '/chats',
+                    badge: '3',
+                    icon: MessagesSquare,
+                },
+                {
+                    title: 'Users',
+                    url: '/users',
+                    icon: Users,
+                },
+                {
+                    title: 'Authentication',
+                    icon: LockKeyhole,
+                    items: [
+                        {
+                            title: 'Sign In',
+                            url: '/auth/sign-in',
+                        },
+                        {
+                            title: 'Sign Up',
+                            url: '/auth/sign-up',
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            title: 'Pages',
+            items: [
+                {
+                    title: 'Auth',
+                    icon: ShieldCheck,
+                    items: [
+                        {
+                            title: 'Sign In',
+                            url: '/sign-in',
+                        },
+                        {
+                            title: 'Sign In (2 Col)',
+                            url: '/sign-in-2',
+                        },
+                        {
+                            title: 'Sign Up',
+                            url: '/sign-up',
+                        },
+                        {
+                            title: 'Forgot Password',
+                            url: '/forgot-password',
+                        },
+                        {
+                            title: 'OTP',
+                            url: '/otp',
+                        },
+                    ],
+                },
+                {
+                    title: 'Errors',
+                    icon: Bug,
+                    items: [
+                        {
+                            title: 'Unauthorized',
+                            url: '/errors/unauthorized',
+                            icon: Lock,
+                        },
+                        {
+                            title: 'Forbidden',
+                            url: '/errors/forbidden',
+                            icon: UserX,
+                        },
+                        {
+                            title: 'Not Found',
+                            url: '/errors/not-found',
+                            icon: FileX,
+                        },
+                        {
+                            title: 'Internal Server Error',
+                            url: '/errors/internal-server-error',
+                            icon: ServerOff,
+                        },
+                        {
+                            title: 'Maintenance Error',
+                            url: '/errors/maintenance-error',
+                            icon: Construction,
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            title: 'Other',
+            items: [
+                {
+                    title: 'Settings',
+                    icon: Settings,
+                    items: [
+                        {
+                            title: 'Profile',
+                            url: '/settings',
+                            icon: UserCog,
+                        },
+                        {
+                            title: 'Account',
+                            url: '/settings/account',
+                            icon: Wrench,
+                        },
+                        {
+                            title: 'Appearance',
+                            url: '/settings/appearance',
+                            icon: Palette,
+                        },
+                        {
+                            title: 'Notifications',
+                            url: '/settings/notifications',
+                            icon: Bell,
+                        },
+                        {
+                            title: 'Display',
+                            url: '/settings/display',
+                            icon: Monitor,
+                        },
+                    ],
+                },
+                {
+                    title: 'Help Center',
+                    url: '/help-center',
+                    icon: HelpCircle,
+                },
+            ],
+        },
+    ],
+}
