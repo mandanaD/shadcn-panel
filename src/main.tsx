1c46416d04459931e248b6c496e2c872da4d6a7f
Mandana Dashti <dmandana59@gmail.com>
2026-01-28 08:39:20 +0330

    feat(router): add RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import './style/index.css'
+import {router} from "@/router.ts";
+import {RouterProvider} from "@tanstack/react-router";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </StrictMode>,
 )
