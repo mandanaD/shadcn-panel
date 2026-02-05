e529e2e2510ffa4d9c6e4f8c47ccb10e8ca7685e
Mandana Dashti <dmandana59@gmail.com>
2026-02-05 17:23:13 +0330

    feat: delete

--- src/routes/_layout.tsx
+++
@@ -1,6 +0,0 @@
-import {createFileRoute} from '@tanstack/react-router'
-import Container from "@/container/Container.tsx";
-
-export const Route = createFileRoute('/_layout')({
-    component: () => <Container/>
-})
