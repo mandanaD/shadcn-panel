d65937af94688972195dbc1c172be650b8550307
Mandana Dashti <dmandana59@gmail.com>
2026-02-05 17:22:57 +0330

    feat: add nuqs adapter and toaster

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,24 @@
 import * as React from 'react'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import {Outlet, createRootRoute} from '@tanstack/react-router'
+import {NuqsAdapter} from "nuqs/adapters/react";
+import {Toaster} from "sonner";
 
 export const Route = createRootRoute({
-  component: RootComponent,
+    component: RootComponent,
 })
 
 function RootComponent() {
-  return (
-    <React.Fragment>
-      <Outlet />
-    </React.Fragment>
-  )
+    return (
+        <React.Fragment>
+            <NuqsAdapter>
+                <Outlet/>
+                <Toaster
+                    position="bottom-right"
+                    richColors={true}
+                    dir={"ltr"}
+                    closeButton={true}
+                />
+            </NuqsAdapter>
+        </React.Fragment>
+    )
 }
