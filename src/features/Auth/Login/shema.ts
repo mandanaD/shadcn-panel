4012c856a8c864d45f357333980bbb475f238817
Mandana Dashti <dmandana59@gmail.com>
2026-02-07 10:47:48 +0330

    feat: delete

--- src/features/Auth/Login/shema.ts
+++
@@ -1,8 +0,0 @@
-import * as z from "zod";
-
-export const loginShema = z.object({
-    email: z.email("Enter valid email address").min(1, "Please enter your email"),
-    password: z.string("Please enter your password").min(7, "Password must be at least 7 characters long"),
-})
-
-export type LoginType=z.infer<typeof loginShema>
