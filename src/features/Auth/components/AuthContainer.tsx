070771b1249e9c9f8f647ec7f6b8d1a65ac7babd
Mandana Dashti <dmandana59@gmail.com>
2026-02-07 10:46:16 +0330

    feat: clear signup session based on URL

--- src/features/Auth/components/AuthContainer.tsx
+++ src/features/Auth/components/AuthContainer.tsx
@@ -1,8 +1,17 @@
 import {BiohazardIcon} from "lucide-react";
+import {useEffect} from "react";
+import {useLocation} from "@tanstack/react-router";
 
 const AuthContainer = ({children}: {
     children: React.ReactNode,
 }) => {
+    const href = useLocation({select: (location) => location.href})
+    useEffect(() => {
+        if (!(href.includes("sign-up"))) {
+            sessionStorage.removeItem("data")
+        }
+    }, []);
+
     return (
         <div className='container flex min-h-svh max-w-none items-center justify-center'>
             <div className='mx-auto flex w-full flex-col justify-center space-y-2 py-8 sm:w-[480px] sm:p-8'>
