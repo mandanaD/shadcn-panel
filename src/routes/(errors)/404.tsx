ce0b0a64eca4883ed1e519b98c63b85b84c6b9a2
Mandana Dashti <dmandana59@gmail.com>
2026-02-13 22:36:09 +0330

    feat: delete 404

--- src/routes/(errors)/404.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/(errors)/404')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/(errors)/404"!</div>
-}
