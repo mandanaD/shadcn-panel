2e31e9591e797a09035defbd7fb7de977c74470d
Mandana Dashti <dmandana59@gmail.com>
2026-02-13 22:37:05 +0330

    feat: add notFound component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,9 +2,11 @@ import * as React from 'react'
 import {Outlet, createRootRoute} from '@tanstack/react-router'
 import {NuqsAdapter} from "nuqs/adapters/react";
 import {Toaster} from "sonner";
+import {NotFoundError} from "@/features/Errors/NotFoundError.tsx";
 
 export const Route = createRootRoute({
     component: RootComponent,
+    notFoundComponent: NotFoundError,
 })
 
 function RootComponent() {
