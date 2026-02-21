f06616fb405bc0d2025efba14f10c203adc11bed
Mandana Dashti <dmandana59@gmail.com>
2026-02-21 21:33:53 +0330

    fix: re-import Settings

--- src/routes/_authenticated/settings/route.tsx
+++ src/routes/_authenticated/settings/route.tsx
@@ -1,5 +1,5 @@
 import {createFileRoute} from '@tanstack/react-router'
-import Settings from "@/features/Settings/Settings.tsx";
+import Settings from "@/features/Settings/pages/Settings.tsx";
 
 export const Route = createFileRoute('/_authenticated/settings')({
   component: RouteComponent,
