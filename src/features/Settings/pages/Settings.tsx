ffaef08f8e3fe1f7c9b4462f87292e5ebea70dbc
Mandana Dashti <dmandana59@gmail.com>
2026-02-22 09:30:53 +0330

    feat: add SidebarItem

--- src/features/Settings/pages/Settings.tsx
+++ src/features/Settings/pages/Settings.tsx
@@ -1,6 +1,8 @@
 import {Main} from "@/components/layout/Main.tsx";
 import {Separator} from "@/components/ui/separator.tsx";
 import {Outlet} from "@tanstack/react-router";
+import {SidebarItem} from "@/features/Settings/components/SidebarItem.tsx";
+import {settingSidebar} from "@/features/Settings/utils/setting.utils.tsx";
 
 const Settings = () => {
     return (
@@ -9,16 +11,16 @@ const Settings = () => {
                 <h1 className='text-2xl font-bold tracking-tight md:text-3xl'>
                     Settings
                 </h1>
-                <p className='text-muted-foreground'>
+                <p className='text-muted-foreground text-base'>
                     Manage your account settings and set e-mail preferences.
                 </p>
             </div>
             <Separator className='my-4 lg:my-6' />
             <div className='flex flex-1 flex-col space-y-2 overflow-hidden md:space-y-2 lg:flex-row lg:space-y-0 lg:space-x-12'>
-                <aside className='top-0 lg:sticky lg:w-1/5'>
-                    {/*<SidebarNav items={sidebarNavItems} />*/}
+                <aside className='top-0 lg:sticky'>
+                    <SidebarItem items={settingSidebar} />
                 </aside>
-                <div className='flex w-full overflow-y-hidden p-1'>
+                <div className='flex w-full overflow-y-auto'>
                     <Outlet />
                 </div>
             </div>
